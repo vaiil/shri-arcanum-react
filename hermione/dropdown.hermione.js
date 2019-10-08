8c1e2f440fddef1918b4b6444997bae05bce90e9
vail <ysywix@gmail.com>
2019-10-09 00:42:33 +0300

    Add navigation tests

--- hermione/dropdown.hermione.js
+++ hermione/dropdown.hermione.js
@@ -4,12 +4,35 @@ describe('Список репозиториев', () => {
   it('Скрыт пока не кликнули', async function () {
     const visible = await this.browser.url('/')
       .isVisible('.RepoSelect-Items')
-    assert.ok(!visible, 'Dropdown не открылся')
+    assert.ok(!visible, 'Dropdown открыт изначально (такого быть не должно)')
   })
   it('Открыт, когда кликнули', async function () {
     const visible = await this.browser.url('/')
       .click('.RepoSelect-Current')
       .isVisible('.RepoSelect-Items')
     assert.ok(visible, 'Dropdown не открылся')
+  })
+  it('Работает переход при клике на репозиторий', async function () {
+    return this.browser.url('/')
+      .click('.RepoSelect-Current')
+      .then(async () => {
+        const link = await this.browser.$('.RepoSelect-Items a').getAttribute('href')
+        const currentUrl = await this.browser.getUrl()
+        assert.notStrictEqual(currentUrl, link)
+      })
+      .click('.RepoSelect-Items a')
+      .waitUntil(async function () {
+        return await this.getUrl() !== await this.getAttribute('.RepoSelect-Items a', 'href')
+      }, 5000, 'Не успешный переход по ссылке')
+  })
+  it('После перехода dropdown закрыт', async function () {
+    const visible = await this.browser.url('/')
+      .click('.RepoSelect-Current')
+      .click('.RepoSelect-Items a')
+      .waitUntil(async function () {
+        return await this.getUrl() !== await this.getAttribute('.RepoSelect-Items a', 'href')
+      }, 5000, 'Успешный переход по ссылке')
+      .isVisible('.RepoSelect-Items')
+    assert.ok(!visible, 'Dropdown остался открытым')
   })
 })
