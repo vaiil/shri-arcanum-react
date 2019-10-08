926818e7301a27e39b06f7dfc117cf86959ad14f
vail <ysywix@gmail.com>
2019-10-08 14:13:52 +0300

    Add repo failure reducer

--- app/redux/reducers/repoReducer.js
+++ app/redux/reducers/repoReducer.js
@@ -2,6 +2,10 @@ import { GET_REPOS_REQUEST, GET_REPOS_FAILURE, GET_REPOS_SUCCESS, SELECT_REPO } 
 
 export default (state = [], action) => {
   switch (action.type) {
+    case GET_REPOS_REQUEST:
+      return { repos: [], current: null }
+    case GET_REPOS_FAILURE:
+      return { repos: [], current: null, error: state.reason }
     case GET_REPOS_SUCCESS:
       return { repos: action.repos, current: null }
     case SELECT_REPO:
