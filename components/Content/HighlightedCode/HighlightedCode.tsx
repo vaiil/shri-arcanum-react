8b849f374f88fb0d6a92d6f0059a053b1d055733
vail <ysywix@gmail.com>
2019-10-15 14:30:37 +0300

    Fix code highlighter

--- components/Content/HighlightedCode/HighlightedCode.tsx
+++ components/Content/HighlightedCode/HighlightedCode.tsx
@@ -1,19 +1,19 @@
-const SyntaxHighlighter = require('react-syntax-highlighter/dist/cjs/default-highlight')
+const SyntaxHighlighter = require('react-syntax-highlighter/dist/cjs/default-highlight').default
 import './HighlightedCode.css'
 import { cnHighlightedCode, HighlightedCodeProps } from './index'
 
-const style = require('react-syntax-highlighter/dist/cjs/styles/hljs/github-gist')
+const style = require('react-syntax-highlighter/dist/cjs/styles/hljs/github-gist').default
 import { FC } from 'react'
 
 const HighlightedCode: FC<HighlightedCodeProps> = ({ code }) => (
   <div
     className={cnHighlightedCode()}>
-    {/*<SyntaxHighlighter language="js"*/}
-    {/*                   style={style}*/}
-    {/*                   customStyle={{ margin: 0 }}*/}
-    {/*                   showLineNumbers={true}>*/}
-    {/*  {code}*/}
-    {/*</SyntaxHighlighter>*/}
+    <SyntaxHighlighter language="js"
+                       style={style}
+                       customStyle={{ margin: 0 }}
+                       showLineNumbers={true}>
+      {code}
+    </SyntaxHighlighter>
   </div>
 )
 export default HighlightedCode
