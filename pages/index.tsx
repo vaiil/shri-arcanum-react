abf9d362e9bec54f8574b4d89340e2d27ca96908
Vladimir Grinenko <i@tadatuta.com>
2019-10-22 01:05:49 +0300

    Add Tree dependency to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import { Layout } from 'components/Common/Layout'
 import { withRedux, NextPageReduxContext } from '../app/redux/withRedux'
 import { fetchRepos, selectRepo } from '../app/redux/actions/repo'
 import { NextComponentType } from 'next'
-
+import dynamic from 'next/dynamic';
+dynamic(() => import('../components/Content/Tree/Tree'));
 
 const Home: NextComponentType<NextPageReduxContext, {}, {}> = () => (
   <Layout title='Index page'/>
